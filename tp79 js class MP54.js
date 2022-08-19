4029ea9a5e7d25838d1e1901f59f9bae59bdc567
Mateo <108905809+MateoPalotta@users.noreply.github.com>
2022-08-19 13:50:45 -0300

    Add files via upload

---
+++ tp79 js class MP54.js
@@ -0,0 +1,10 @@
+class CRectangulo{
+    constructor(alto, longitud){
+        this.alto = alto;
+        this.longitud = longitud
+    }
+    CalcularArea(alto, longitud){
+        return alto*longitud;
+    }
+
+}
